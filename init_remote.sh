d7234b6d4666674b1d1a73e32f1c9865c67fb551
firmianay <firmianay@gmail.com>
2022-03-02 15:29:53 +0800

    fix

--- init_remote.sh
+++ init_remote.sh
@@ -6,14 +6,25 @@ adb root
 VER=$(frida --version)
 ABI=$(adb shell getprop ro.product.cpu.abi)
 if [[ $ABI == *arm64* ]]; then
-    ABI="arm64"
+    frida_ABI="arm64"
+    other_ABI="aarch64"
 fi
 
-wget -q https://github.com/frida/frida/releases/download/$VER/frida-server-$VER-android-$ABI.xz -O frida-server.xz
+wget -q https://github.com/frida/frida/releases/download/$VER/frida-server-$VER-android-$frida_ABI.xz -O frida-server.xz
 xz -d frida-server.xz
-adb push frida-server /data/local/tmp/
-adb shell "chmod 755 /data/local/tmp/frida-server"
-rm frida-server
+chmod +x frida-server && adb push frida-server /data/local/tmp/
+
+wget -q https://github.com/ernw/static-toolbox/releases/download/tcpdump-v4.9.3/tcpdump-4.9.3-$other_ABI -O tcpdump
+chmod +x tcpdump && adb push tcpdump /data/local/tmp/
+
+wget -q https://github.com/ernw/static-toolbox/releases/download/nmap-v7.91SVN/nmap-7.91SVN-$other_ABI-portable.zip -O nmap.zip
+adb push nmap.zip /data/local/tmp/
+adb shell "cd /data/local/tmp && unzip -q nmap.zip -d nmap && rm nmap.zip"
+
+wget -q https://github.com/ernw/static-toolbox/releases/download/gdb-v10.1/gdbserver-10.1-$other_ABI -O gdbserver
+chmod +x gdbserver && adb push gdbserver /data/local/tmp/
+
+rm frida-server tcpdump nmap.zip gdbserver
 
 adb install ./tools/drozer/drozer.apk
 adb install ./tools/wadb.apk
