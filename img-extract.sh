68e94312245f0759c44ea5fcc70fbaac0e34c527
firmianay <firmianay@gmail.com>
2023-08-21 15:29:44 +0800

    fix

--- img-extract.sh
+++ img-extract.sh
@@ -5,12 +5,14 @@
 # $ simg2img ./data/super.img ./data/super.img_raw
 #
 # 从 raw image 提取分区镜像文件
-# $ mkdir ./data/system ./data/vendor
+# $ mkdir ./data/system ./data/system_ext ./data/vendor ./data/product
 # $ ./tools/lpunpack ./data/super.img_raw ./data
 #
 # 挂载镜像文件
 # $ sudo mount -o ro ./data/system_a.img ./data/system
+# $ sudo mount -o ro ./data/system_ext_a.img ./data/system_ext
 # $ sudo mount -o ro ./data/vendor_a.img ./data/vendor
+# $ sudo mount -o ro ./data/product_a.img ./data/product
 #
 # 搜索所有 APK
 # $ find ./data -name "*.apk" 2>/dev/null
@@ -28,33 +30,46 @@ extract () {
     rm $dirpath/super.img
 
     echo "[+] Extracting partition images from super"
-    mkdir -p $dirpath/system $dirpath/vendor
+    mkdir -p $dirpath/system $dirpath/system_ext $dirpath/vendor $dirpath/product
     ./tools/lpunpack $dirpath/super.img_raw $dirpath
     rm $dirpath/super.img_raw
 
     sudo mount -o ro $dirpath/system_a.img $dirpath/system
+    sudo mount -o ro $dirpath/system_ext_a.img $dirpath/system_ext
     sudo mount -o ro $dirpath/vendor_a.img $dirpath/vendor
+    sudo mount -o ro $dirpath/product_a.img $dirpath/product
 
     echo "[+] Extracting APK files"
-    APK_DIR="$dirpath/apk"
+    APK_DIR=$dirpath/apk
     APK_LIST=$APK_DIR/apk_list.txt
     LIB_LIST=$APK_DIR/lib_list.txt
-    mkdir -p $APK_DIR/system/product $APK_DIR/vendor $APK_DIR/libjni
+    mkdir -p $APK_DIR/system/product $APK_DIR/system_ext $APK_DIR/vendor $APK_DIR/product $APK_DIR/libjni
     cp -rL $dirpath/system/system/app $APK_DIR/system 2>&1 | cut -d "'" -f 2 > $LIB_LIST
     cp -rL $dirpath/system/system/priv-app $APK_DIR/system 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
-    cp -rL $dirpath/system/system/product/app $APK_DIR/system/product 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
-    cp -rL $dirpath/system/system/product/priv-app $APK_DIR/system/product 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    # cp -rL $dirpath/system/system/apex $APK_DIR/system 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    cp -rL $dirpath/system/system/prebuilt $APK_DIR/system 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    # cp -rL $dirpath/system/system/framework/framework-res.apk $APK_DIR/system 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    # cp -rL $dirpath/system/system/product/app $APK_DIR/system/product 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    # cp -rL $dirpath/system/system/product/priv-app $APK_DIR/system/product 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    cp -rL $dirpath/system_ext/app $APK_DIR/system_ext 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    cp -rL $dirpath/system_ext/priv-app $APK_DIR/system_ext 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
     cp -rL $dirpath/vendor/app $APK_DIR/vendor 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    cp -rL $dirpath/product/app $APK_DIR/product 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    cp -rL $dirpath/product/priv-app $APK_DIR/product 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
+    cp -rL $dirpath/product/overlay $APK_DIR/product 2>&1 | cut -d "'" -f 2 >> $LIB_LIST
     find $APK_DIR -name "*.apk" -exec md5sum {} + > $APK_LIST
 
     echo "[+] Extracting lib files"
     while read -r line
     do
-        if [[ $line == *"$dirpath/system"* ]]; then
+        if [[ $line == *"$dirpath/system/"* ]]; then
             remote_path=$dirpath/system$(file $line | cut -d " " -f 6)
             local_path=$APK_DIR${line#*"$dirpath/system"}
-        elif [[ $line == *"$dirpath/vendor"* ]]; then
-            remote_path=$dirpath/vendor$(file $line | cut -d " " -f 6)
+        elif [[ $line == *"$dirpath/vendor/"* ]]; then
+            remote_path=$dirpath$(file $line | cut -d " " -f 6)
+            local_path=$APK_DIR${line#*"$dirpath"}
+        elif [[ $line == *"$dirpath/product/"* ]]; then
+            remote_path=$dirpath$(file $line | cut -d " " -f 6)
             local_path=$APK_DIR${line#*"$dirpath"}
         else
             echo "cannot found $line"
@@ -62,8 +77,10 @@ extract () {
         cp $remote_path $local_path
     done < $LIB_LIST
 
-    find $dirpath/system/ -type f -name "lib*jni*.so" 2>/dev/null > $LIB_LIST
-    find $dirpath/vendor/ -type f -name "lib*jni*.so" 2>/dev/null >> $LIB_LIST
+    find $dirpath/system -type f -name "lib*jni*.so" 2>/dev/null > $LIB_LIST
+    find $dirpath/system_ext -type f -name "lib*jni*.so" 2>/dev/null >> $LIB_LIST
+    find $dirpath/vendor -type f -name "lib*jni*.so" 2>/dev/null >> $LIB_LIST
+    find $dirpath/product -type f -name "lib*jni*.so" 2>/dev/null >> $LIB_LIST
     while read -r line
     do
         local_path=$APK_DIR/libjni/`echo $line | awk -F / '{print $(NF-2)"_"$(NF-1)"_"$NF}'`
@@ -71,9 +88,8 @@ extract () {
     done < $LIB_LIST
     find $APK_DIR -name "*.so" -exec md5sum {} + > $LIB_LIST
 
-    sudo umount $dirpath/system
-    sudo umount $dirpath/vendor
-    rm -rf $dirpath/system $dirpath/vendor $dirpath/*.img
+    sudo umount $dirpath/system $dirpath/system_ext $dirpath/vendor $dirpath/product
+    rm -rf $dirpath/system $dirpath/system_ext $dirpath/vendor $dirpath/product $dirpath/*.img
     echo "[+] Done"
 }
 
@@ -89,25 +105,14 @@ if [ $# -eq 1 ]; then
         echo "[+] Unzipping fastboot"
         unzip -q $1 -d ./data
 
-        if [[ $1 == *8155F* ]]; then
-            dirpath="./data/image_f_$today"
-            mkdir -p $dirpath
+        dirpath="./data/image_$today"
+        mkdir -p $dirpath
 
-            cp ./data/HU_8155F_X01_fastboot/images/super.img $dirpath
-            rm -rf ./data/HU_8155F_X01_fastboot
-            extract
-        elif [[ $1 == *8155R* ]]; then
-            dirpath="./data/image_r_$today"
-            mkdir -p $dirpath
-
-            cp ./data/HU_8155R_X01_fastboot/images/super.img $dirpath
-            rm -rf ./data/HU_8155R_X01_fastboot
-            extract
-        else
-            help
-        fi
+        cp ./data/HU_fastboot/images/super.img $dirpath
+        rm -rf ./data/HU_fastboot
+        extract
     elif [ ${1:0-3} == "img" ]; then
-        dirpath="./data/image_x_$today"
+        dirpath="./data/image_$today"
         mkdir -p $dirpath
 
         cp $1 $dirpath/super.img
